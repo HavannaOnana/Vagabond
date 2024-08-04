544d61590ca10eac70a0994c28cc76aaf2e4cbe3
HavannaOnana <138626351+HavannaOnana@users.noreply.github.com>
2024-08-04 20:21:20 +0200

    added the author

--- main.js
+++ main.js
@@ -21,6 +21,7 @@ vagabondTitle.innerHTML = `
   <div class="v-text">
            <h1>Vagabond</h1>
            <p>Vagabond (Japanese: バガボンド, Hepburn: Bagabondo) is a Japanese<br> epic martial arts manga series written and illustrated by Takehiko Inoue. It portrays a<br> fictionalized account of the life of Japanese swordsman Musashi Miyamoto<br>, based on Eiji Yoshikawa's novel Musashi.</p>
+           <p class="author"> - Takehiko Inoue</p>
         </div>
         <img src="/images/mandk.png" alt="">
 `
